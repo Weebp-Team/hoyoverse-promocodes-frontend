58f259f7ee0eb94a192247d6d21a2651acefd4a3
Nikita <99484973+Nikita-Isaichenko@users.noreply.github.com>
2024-05-15 04:05:04 +0700

    feature(src): добавлена сортировка промокодов

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />,
     },
     {
-        path: '/vk',
+        path: '/vk:code',
         element: <VkAuthorizationPage />,
     },
 ]);
